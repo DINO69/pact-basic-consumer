ab556d796ea0e363d2e3e9980c8d3dbf74b0e13f
Guilherme <guilherme.fernandes@inmetrics.com.br>
2021-12-03 12:41:06 -0300

    add class test

---
+++ src/test/java/br/ce/wcaquino/consumer/tasks/service/TasksConsumerTest.java
@@ -0,0 +1,14 @@
+package br.ce.wcaquino.consumer.tasks.service;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TasksConsumerTest {
+
+    @Test
+    public void testNumberOne(){
+        Assert.assertEquals(true,true);
+    }
+
+}
